164babea48d2d93af1a20c875cbafb8a7b477bdb
ALVINO ALBAS <94214515+herplagi@users.noreply.github.com>
2022-03-03 14:21:21 +0700

    Delete Tugas SDA directory

--- Tugas SDA/src/Nomor2.java
+++
@@ -1,9 +0,0 @@
-public class Nomor2 {
-    public static void main(String[] args) {
-        String [] nama = {"L","B","A","S"};
-        // soal no 2
-        //Tentukan hasil yang didapatkan ketika dilakukan perintah berikut ini:
-        System.out.println("Ukuran array adalah " + nama.length);
-
-    }
-}
